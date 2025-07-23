a06c248541e9358d1b9616f7b30357a9ddadd374
Erfan Manouchehri <erfan2004@yahoo.com>
2025-07-23 19:41:49 +0330

    coded the app and set it up

---
+++ Backend/app.js
@@ -0,0 +1,10 @@
+import express from "express";
+import authRoutes from "./routes/authRoutes";
+
+
+const app = express();
+
+app.use(express.json());
+app.use('/api/auth', authRoutes);
+
+module.exports = app;
