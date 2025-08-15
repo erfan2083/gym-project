3f5a98409e1baad267eba68816951cd30185941d
Erfan Manouchehri <erfan2004@yahoo.com>
2025-08-15 23:32:52 +0330

    added the role parametr

--- mobile-app/utils/api.js
+++ mobile-app/utils/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_URL = 'http://localhost:3000/api'; 
 
 export const signup = async (fullName, email, password) => {
-  return await axios.post(`${API_URL}/auth/signup`, { fullName, email, password });
+  return await axios.post(`${API_URL}/auth/signup`, { fullName, email, password, rool });
 };
 
 export const login = async (email, password) => {
