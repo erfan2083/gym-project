83f3dc481bf75a428cab61be4fbe882ee4c6f825
Erfan Manouchehri <erfan2004@yahoo.com>
2025-09-26 16:29:41 +0330

    Added the routes

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,10 +1,13 @@
 import express from "express";
 import { signup, login } from "../controllers/authController.js";
+import { createCoachProfile, updateCoachProfile } from "../controllers/couchController.js"
 
 
 const router = express.Router();
 
 router.post('/signup', signup);
 router.post('/login', login);
+router.post('/CreateCouchProfile', createCoachProfile);
+router.post('/UpdateCoachProfile', updateCoachProfile);
 
 export default router;
