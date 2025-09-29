b114f8d05efe009580f48171326a2403e9c57276
Erfan Manouchehri <erfan2004@yahoo.com>
2025-09-29 19:24:38 +0330

    added get couches

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { signup, login } from "../controllers/authController.js";
-import { createCoachProfile, updateCoachProfile } from "../controllers/couchController.js"
+import { createCoachProfile, updateCoachProfile, getCoaches } from "../controllers/couchController.js"
 
 
 const router = express.Router();
@@ -9,5 +9,6 @@ router.post('/signup', signup);
 router.post('/login', login);
 router.post('/CreateCouchProfile', createCoachProfile);
 router.post('/UpdateCoachProfile', updateCoachProfile);
+router.get('/getCouches', getCoaches);
 
 export default router;
