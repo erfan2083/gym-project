f8cd51f156b9027e2921cbb62d36259b76a35f83
Erfan Manouchehri <erfan2004@yahoo.com>
2025-10-29 18:37:16 +0330

    updated router

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,14 +1,17 @@
-import express from "express";
-import { signup, login } from "../controllers/authController.js";
-import { createCoachProfile, updateCoachProfile, getCoaches } from "../controllers/couchController.js"
-
+// routes/authRoutes.js
+import express from 'express';
+import {
+  signupStart,
+  signupVerify,
+  signupComplete,
+  login
+} from '../controllers/authController.js';
 
 const router = express.Router();
 
-router.post('/signup', signup);
+router.post('/signup/start', signupStart);
+router.post('/signup/verify', signupVerify);
+router.post('/signup/complete', signupComplete);
 router.post('/login', login);
-router.post('/CreateCouchProfile', createCoachProfile);
-router.post('/UpdateCoachProfile', updateCoachProfile);
-router.get('/getCouches', getCoaches);
 
 export default router;
